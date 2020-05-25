0892a4de2dcfcb3f6b4fcacb1fe36e6d64148dd4
Subha Maity <smaity@smaity-mbp.local>
2020-05-25 14:30:22 -0400

    new file:   experiment.py new file:   nn_graph.py new file:   setup.py new file:   test.ipynb new file:   toxicity_model.py new file:   utils.py

---
+++ group-fairness/toxicity/nn_graph.py
@@ -0,0 +1,93 @@
+import tensorflow as tf
+from tensorflow import keras
+
+
+
+class NNGraph(keras.Model):
+
+    """
+    Class for neural net graph
+
+    initialization: 
+
+    architecture: (list of int) list of neuron numbers in each layers; one layer is equivalent to logistic regression
+    activation: (Tensorflow Core function) activation function for hidden layers
+    input_shape: (tuple) provide input shape; if vector provide (dimension, ) 
+
+
+    variables:
+
+    Layers: (list of layers from keras.layers API) contains all the layers
+    model: (keras.model.Sequential object)
+
+
+    """
+
+    # Set layers
+    def __init__(self, architecture = [10, 20, 2], activation = tf.nn.relu, input_shape = (40,), convex = False, name = None):
+        super(NNGraph, self).__init__()
+        self.Layers = []
+        
+        if convex:
+            kernel_constraint = keras.constraints.NonNeg()
+            kernel_initializer = tf.random_uniform_initializer(minval = 0, maxval = 0.1, seed = 1)
+        else:
+            kernel_constraint = None
+            kernel_initializer = None
+
+        if len(architecture) == 1:
+            self.Layers.append(keras.layers.Dense(architecture[0], input_shape = input_shape,\
+                 kernel_constraint = kernel_constraint, kernel_initializer = kernel_initializer, name = f'logit-layer'))
+
+        elif len(architecture) == 2: 
+            self.Layers.append(keras.layers.Dense(architecture[0], activation = activation, \
+                    kernel_constraint = kernel_constraint, kernel_initializer = kernel_initializer,\
+                         input_shape = input_shape, name = f'hidden-layer-1'))
+
+            self.Layers.append(keras.layers.Dense(architecture[1], kernel_constraint = kernel_constraint,\
+                 kernel_initializer = kernel_initializer, name = 'logit-layer'))
+
+
+        elif len(architecture) == 3:
+            self.Layers.append(keras.layers.Dense(architecture[0], activation = activation, \
+                    kernel_constraint = kernel_constraint, kernel_initializer = kernel_initializer,\
+                         input_shape = input_shape, name = f'hidden-layer-1'))
+
+            self.Layers.append(keras.layers.Dense(architecture[1], activation = activation, \
+                    kernel_constraint = kernel_constraint, kernel_initializer = kernel_initializer,\
+                         input_shape = input_shape, name = f'hidden-layer-2'))
+
+            self.Layers.append(keras.layers.Dense(architecture[2],\
+                kernel_constraint = kernel_constraint, kernel_initializer = kernel_initializer,\
+                     name = 'logit-layer'))
+
+
+        else:
+            self.Layers.append(keras.layers.Dense(architecture[0], activation = activation, \
+                    kernel_constraint = kernel_constraint, kernel_initializer = kernel_initializer,\
+                         input_shape = input_shape, name = f'hidden-layer-1'))
+
+            for layer_index, neurons in enumerate(architecture[1:-1], 2):
+                    self.Layers.append(keras.layers.Dense(neurons, activation = activation, \
+                                kernel_constraint = kernel_constraint, kernel_initializer = kernel_initializer,\
+                                     name = f'hidden-layer-{layer_index}'))
+
+            self.Layers.append(keras.layers.Dense(architecture[-1],\
+                 kernel_constraint = kernel_constraint, kernel_initializer = kernel_initializer,\
+                      name = 'logit-layer'))
+
+        
+      
+        self.model = keras.models.Sequential(self.layers, name = name)
+
+    # Set forward pass
+    def call(self, x, probability = False):
+        out = self.model(x)
+
+        if probability:
+            out = tf.nn.softmax(out)
+            out, _ = tf.linalg.normalize(out, ord = 1, axis = 1) # normalizes softmax vector to get probabilities 
+        
+        return out
+
+
---
+++ group-fairness/toxicity/toxicity_model.py
@@ -0,0 +1,61 @@
+import tensorflow as tf
+from tensorflow import keras
+import io
+import os
+import shutil
+import sys
+import tempfile
+import zipfile
+from urllib import request
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import time
+
+
+from tensorflow import keras
+
+
+
+
+
+
+
+def toxicity_model(embedding_matrix, hparams = {\
+    "cnn_filter_sizes": [128, 128, 128],\
+    "cnn_kernel_sizes": [5, 5, 5],\
+    "cnn_pooling_sizes": [5, 5, 40],\
+    "embedding_dim": 100,\
+    "embedding_trainable": False,\
+    "max_sequence_length": 250\
+}\
+):
+    model = keras.Sequential()
+
+    # Embedding layer.
+    embedding_layer = keras.layers.Embedding(
+        embedding_matrix.shape[0], embedding_matrix.shape[1], weights=[embedding_matrix],
+        input_length=hparams["max_sequence_length"], trainable=hparams['embedding_trainable'])
+    model.add(embedding_layer)
+
+    # Convolution layers.
+    for filter_size, kernel_size, pool_size in zip(
+        hparams['cnn_filter_sizes'], hparams['cnn_kernel_sizes'], hparams['cnn_pooling_sizes']):
+
+        conv_layer = keras.layers.Conv1D(filter_size, kernel_size, activation='relu', padding='same')
+        model.add(conv_layer)
+
+        pooled_layer = keras.layers.MaxPooling1D(pool_size, padding='same')
+        model.add(pooled_layer)
+
+    # Add a flatten layer, a fully-connected layer and an output layer.
+    model.add(keras.layers.Flatten())
+    model.add(keras.layers.Dense(128, activation='relu'))
+    model.add(keras.layers.Dense(1))
+
+    return model
+
+
+
+
